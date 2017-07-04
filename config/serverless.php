61fb5e917ea573f48689a1bc1a84bc87d30c8726
Bubba <rob@hines57.com>
2017-07-04 10:20:05 -0400

    Consolidate the ignore patterns.

--- config/serverless.php
+++ config/serverless.php
@@ -2,28 +2,26 @@
 return [
     'packaging' => [
         'ignore' => [
-            'directories' => [
-                'vendor',
-                'tests',
-                'storage',
-                '.idea',
-                '.git',
-                'resources/lib/php',
-                'resources/lib/pkgconfig',
-                'resources/dist'
-            ],
-            'files' => [
-                '.gitignore',
-                '.env',
-                '.env.example',
-                '.gitkeep',
-                '.htaccess',
-                'readme.md',
-                'versions.json',
-                '.php_cs.cache',
-                'composer.json',
-                'composer.lock'
-            ]
+            // Directories & Fully Qualified Paths
+            base_path('vendor'),
+            base_path('tests'),
+            base_path('storage'),
+            base_path('.idea'),
+            base_path('.git'),
+            base_path('resources/lib/php'),
+            base_path('resources/lib/pkgconfig'),
+            base_path('resources/dist'),
+            // Files
+            '.gitignore',
+            '.env',
+            '.env.example',
+            '.gitkeep',
+            '.htaccess',
+            'readme.md',
+            'versions.json',
+            '.php_cs.cache',
+            'composer.json',
+            'composer.lock'
         ],
         'executables' => [
             'resources/bin/php-cgi'
