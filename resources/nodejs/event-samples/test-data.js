b80a58b747fbcbcbc123ad69283c9d4b7da38a19
Bubba <rob@hines57.com>
2017-07-04 14:57:44 -0400

    Create a sample event for testing.

---
+++ resources/nodejs/event-samples/test-data.js
@@ -0,0 +1,15 @@
+module.exports = {
+    queryStringParameters: {
+        foo: "bar",
+        bar: "foo"
+    },
+    headers: {
+        host: "lambda_test.dev",
+        Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "en-US,en;q=0.8",
+        "Cache-Control": "max-age=0",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36"
+    },
+    path: "/laravel-aws-lambda/info"
+};
